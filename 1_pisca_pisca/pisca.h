1a09d792d5730139de0dcafb45d515c85e50ef77
Thiago Condé <thiagocondiolov@hotmail.com>
2022-02-07 15:25:10 -0300

    ajustes

--- 1_pisca_pisca/pisca.h
+++ 1_pisca_pisca/pisca.h
@@ -1,8 +1,10 @@
-#define LED_PISCA 15    //D4(2) ou D0(16) ou D8(15) colocando led
+#define LED_PISCA 15 //D4(2) ou D0(16) ou D8(15) colocando led
 
 void pisca (){
+
   digitalWrite(LED_PISCA, LOW);
   delay(1000);
+
   digitalWrite(LED_PISCA, HIGH);
   delay(1000);
 }
